f619794f343a731b049461abe12562c26953574c
Harsh Vardhan <59030402+hvhawk@users.noreply.github.com>
2022-04-12 03:52:04 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,13 @@
 mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"email@domain\"\n\
+" > ~/.streamlit/credentials.toml
+
 echo "\
 [server]\n\
 headless = true\n\
-port = $PORT\n\
 enableCORS=false\n\
+port = $PORT\n\
 " > ~/.streamlit/config.toml
